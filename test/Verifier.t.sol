f6869842d5acaea5f8d6d65053eac1918b8faf3f
Ian-Bright <ianbrighton1@gmail.com>
2023-07-03 14:42:15 -0600

    extract public inputs and verify onchain

--- test/Verifier.t.sol
+++ test/Verifier.t.sol
@@ -2,31 +2,30 @@
 pragma solidity <0.9;
 
 import "forge-std/Test.sol";
+import {VerifierWrapper} from "../contracts/VerifierWrapper.sol";
 import {YulDeployer} from "../contracts/YulDeployer.sol";
 
 contract YulDeployerTest is Test {
     address verifierAddress;
+    VerifierWrapper verifierWrapper;
     YulDeployer yulDeployer;
 
     function setUp() public {
         yulDeployer = new YulDeployer();
-
         verifierAddress = address(
             yulDeployer.deployContract("../snark-verifiers/9_public_io")
         );
+        verifierWrapper = new VerifierWrapper(verifierAddress);
     }
 
-    function verifyProof(bytes memory input) private returns (bool) {
-        (bool success, ) = verifierAddress.call(input);
-        return success;
-    }
-
-    function test_yulVerifier_verify() external {
+    function test_verifier() external {
+        // Read in proof from calldata file
         string memory proofStr = vm.readFile(
             "snark-verifiers/data/9_public_io.calldata"
         );
-        bytes memory proofData = vm.parseBytes(proofStr);
-        bool success = verifyProof(proofData);
-        assert(success);
+        bytes memory proof = vm.parseBytes(proofStr);
+
+        // Verify proof and assert success
+        uint256 num = verifierWrapper.validProof(7, proof);
     }
 }
